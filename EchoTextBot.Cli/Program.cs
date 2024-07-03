14437c57e3a95ce75cde59ea708ca521717b6832
Anton Bauer <bauer_av+git@proton.me>
2024-07-03 17:32:03 +0200

    initial solution creation

---
+++ EchoTextBot.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// get messages
+// fiilter everything but audio
+
+// convert to format used by whisper
+
+// transcribe by whisper
+// output to file
+
+// Answer to original message
