ea30fac0c5c9983bc00d32c7ee8601e15ccc7ab4
Anton Bauer <bauer_av+git@proton.me>
2024-07-04 22:22:17 +0200

    implement basic updates handling loop

--- EchoTextBot.Cli/Program.cs
+++ EchoTextBot.Cli/Program.cs
@@ -1,7 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.GettingUpdates;
 
 // get messages
+var botToken = "";
+var client = new TelegramBotClient(botToken);
+
+var updates = await client.GetUpdatesAsync();
+while(true)
+{
+    if (updates.Any())
+    {
+        foreach (var update in updates)
+        {
+            if(update.Message is null) continue;
+            await client.SendMessageAsync(update.Message.Chat.Id, update.Message?.Text ?? "Strange update");
+        }
+
+        var offset = updates.Last().UpdateId + 1;
+        updates = await client.GetUpdatesAsync(offset);
+    }
+    else
+    {
+        updates = await client.GetUpdatesAsync();
+    }
+}
 // fiilter everything but audio
 
 // convert to format used by whisper
