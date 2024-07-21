bcc893ae458cd636a0bc2a7dec5bc9a985203a16
Anton Bauer <bauer_av+git@proton.me>
2024-07-21 15:49:22 +0200

    implement telegram bot abstraction

--- TgBot/TelegramBot.cs
+++ TgBot/TelegramBot.cs
@@ -5,12 +5,66 @@ public sealed class TelegramBot
     private readonly TelegramBotClient _tgClient;
     private readonly BotSettings _settings;
     private readonly HttpClient _httpClient;
+    private readonly OffsetService _offsetService;
+
+    private readonly Func<Update, bool> _isUpdateRelevant;
+    private readonly Func<Update, Task> _updateProcessor;
+
+    private TelegramBot(TelegramBotClient tgClient,
+                        BotSettings settings,
+                        HttpClient httpClient,
+                        OffsetService offsetService,
+                        Func<Update, bool> isUpdateRelevant,
+                        Func<Update, Task> updateProcessor)
+    {
+        _tgClient = tgClient;
+        _settings = settings;
+        _httpClient = httpClient;
+        _offsetService = offsetService;
+        _isUpdateRelevant = isUpdateRelevant;
+        _updateProcessor = updateProcessor;
+    }
+
+    public static TelegramBot Create(Func<Update, bool> isUpdateRelevant,
+                                     Func<Update, Task> updateProcessor)
+    {
+        var settings = BotSettings.Create();
+        var tgClient = new TelegramBotClient(settings.TgToken);
+        var httpClient = new HttpClient();
+        var offsetService = new OffsetService(settings.DbConnectionString);
+
+        return new(tgClient,
+                   settings,
+                   httpClient,
+                   offsetService,
+                   isUpdateRelevant,
+                   isUpdateRelevant);
+    }
 
     public async Task Run(CancellationToken cancellationToken)
     {
+        var offset = await _offsetService.ReadOffset(cancellationToken);
+
         while (!cancellationToken.IsCancellationRequested)
         {
+            var allUpdates = await LoadUpdates(offset, cancellationToken);
+            if (allUpdates.Length > 0)
+            {
+                foreach (var update in allUpdates.Where(_isUpdateRelevant))
+                {
+                    await _updateProcessor(update);
+                }
 
+                offset = allUpdates[^1].UpdateId + 1;
+                await _offsetService.SaveOffset(offset.Value, cancellationToken);
+                continue;
+            }
+
+            await Task.Delay(_settings.FetchIntervalMs, cancellationToken);
         }
     }
+
+    private async Task<Update[]> LoadUpdates(int? offset,
+                                             CancellationToken cancellationToken) =>
+        (await tgClient.GetUpdatesAsync(offset, cancellationToken: cancellationToken)).ToArray();
 }
